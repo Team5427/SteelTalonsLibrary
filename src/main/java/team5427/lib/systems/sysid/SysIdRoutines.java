7319584edc1b267eca4741e8046d66e03034c96b
Saathvik k <saathvikkamma@gmail.com>
2025-04-12 16:15:34 -0500

    added swerve drive system classes for easy configuration of a swerve drive based on an interface template

---
+++ src/main/java/team5427/lib/systems/sysid/SysIdRoutines.java
@@ -0,0 +1,14 @@
+package team5427.lib.systems.sysid;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+
+public class SysIdRoutines {
+  
+}
