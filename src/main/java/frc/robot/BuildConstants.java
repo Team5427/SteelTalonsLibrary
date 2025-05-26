c350fcff1488755b0ab81470b2bdd8193c9f32a6
Saathvik k <saathvikkamma@gmail.com>
2025-05-26 10:41:30 -0500

    update git ignore

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -1,17 +1,19 @@
 package frc.robot;
 
-/** Automatically generated file containing build version information. */
+/**
+ * Automatically generated file containing build version information.
+ */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "SteelTalonsLibrary";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 10;
-  public static final String GIT_SHA = "d7ac727034917142363686c5fc515d5e74a2daaf";
-  public static final String GIT_DATE = "2025-05-25 23:28:17 CDT";
+  public static final int GIT_REVISION = 11;
+  public static final String GIT_SHA = "705e49c345aa11edc70a39011f5b165198081ec2";
+  public static final String GIT_DATE = "2025-05-26 00:38:19 CDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-05-26 00:21:06 CDT";
-  public static final long BUILD_UNIX_TIME = 1748236866695L;
+  public static final String BUILD_DATE = "2025-05-26 10:39:58 CDT";
+  public static final long BUILD_UNIX_TIME = 1748273998862L;
   public static final int DIRTY = 1;
 
-  private BuildConstants() {}
+  private BuildConstants(){}
 }
