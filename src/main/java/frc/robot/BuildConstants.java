6c04321b63fc24a6f1282aca6b15c4c869f60015
AaronZhenger <alock8mini@gmail.com>
2025-12-31 22:57:18 -0600

    choreo folder + versioning

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "SteelTalonsLibrary";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 24;
-  public static final String GIT_SHA = "c1f914b46923da8a27bc2789bc3898e721b5c0fe";
-  public static final String GIT_DATE = "2025-10-21 18:44:44 CDT";
-  public static final String GIT_BRANCH = "Learning2025Code";
-  public static final String BUILD_DATE = "2025-10-21 19:05:39 CDT";
-  public static final long BUILD_UNIX_TIME = 1761091539912L;
-  public static final int DIRTY = 0;
+  public static final int GIT_REVISION = 37;
+  public static final String GIT_SHA = "7be6d94f06d7f0d9fe683ed01fdb80ec7763b53c";
+  public static final String GIT_DATE = "2025-12-31 22:53:54 CST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2025-12-31 22:55:17 CST";
+  public static final long BUILD_UNIX_TIME = 1767243317973L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
