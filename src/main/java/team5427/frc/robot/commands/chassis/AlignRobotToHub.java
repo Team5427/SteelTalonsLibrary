4eaf1c8b4142e8ae4be4b77d2c8c8cfe2dab748e
da-supreme-n0ob <harrychess3@gmail.com>
2026-01-22 19:19:27 -0600

    updated align to hub

--- src/main/java/team5427/frc/robot/commands/chassis/AlignRobotToHub.java
+++ src/main/java/team5427/frc/robot/commands/chassis/AlignRobotToHub.java
@@ -2,6 +2,7 @@ package team5427.frc.robot.commands.chassis;
 
 import static edu.wpi.first.units.Units.Meter;
 import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.MetersPerSecond;
 
 import com.pathplanner.lib.util.DriveFeedforwards;
 
@@ -13,26 +14,34 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.LinearVelocity;
+import edu.wpi.first.units.measure.Velocity;
 import edu.wpi.first.wpilibj.drive.RobotDriveBase;
 import edu.wpi.first.wpilibj2.command.Command;
+import team5427.frc.robot.Constants;
+import team5427.frc.robot.Constants.DriverConstants;
 import team5427.frc.robot.subsystems.Swerve.SwerveSubsystem;
 
 public class AlignRobotToHub extends Command{
     private SwerveSubsystem swerveSubsystem;
 
     private final Translation2d targetPose = new Translation2d(5,7); 
-    private final Rotation2d targetAngle = Rotation2d.fromRadians(Math.PI);
 
     private Rotation2d rotation;
     private Translation2d robotPose;
+    private double robotVelocityX;
+    private double robotVelocityY;//meters per second
 
-    private final PIDController pidController = new PIDController(1,1,1); //not sure what to input in the parameters
+    private final PIDController pidRotation = new PIDController(0.05, 0, 0); //random numbers
+
     private final SimpleMotorFeedforward feedforward = new SimpleMotorFeedforward(1, 1);
 
-    public AlignRobotToHub(Pose2d robotPose2d){
+    public AlignRobotToHub(Pose2d robotPose2d, double velocityX, double velocityY){
         swerveSubsystem = SwerveSubsystem.getInstance();
 
         this.robotPose = robotPose2d.getTranslation();
+        this.robotVelocityX = velocityX;
+        this.robotVelocityY = velocityY;
         this.rotation = robotPose2d.getRotation();
     }
 
@@ -45,15 +54,29 @@ public class AlignRobotToHub extends Command{
     public void execute(){
         ChassisSpeeds driverSpeeds;
 
-        Translation2d poseDistance = targetPose.minus(robotPose);
+        //robots x and y distance from the hub
+        double dX = targetPose.getX()-robotPose.getX();
+        double dY = targetPose.getY()-robotPose.getY();
+        
+        //predicts the robots next future pose
+        double vX = robotVelocityX;
+        double vY = robotVelocityY;
+
+        dX = dX+vX*Constants.kLoopSpeed; 
+        dY = dY+vY*Constants.kLoopSpeed;
+
+        //angle needed to align the robot to the hub
+        Rotation2d targetAngle = Rotation2d.fromRadians(Math.atan2(dX,dY));
+        //difference between the robot's current angle and the required angle
         Rotation2d angleDifference = targetAngle.minus(rotation);
 
-        double vX = pidController.calculate(poseDistance.getMeasureX().in(Meters));
-        double vY = pidController.calculate(poseDistance.getMeasureY().in(Meters));
-        double omegaRadians = pidController.calculate(angleDifference.getRadians());
+        //recalculation using PID to account for real world errors
+        double omegaRadians = pidRotation.calculate(angleDifference.getRadians(), 0);
 
-        driverSpeeds = new ChassisSpeeds(vX,vY,omegaRadians);
+        //rotates the robot
+        driverSpeeds = new ChassisSpeeds(0,0,omegaRadians);
         swerveSubsystem.setInputSpeeds(driverSpeeds);
+        
     }
 
     @Override
