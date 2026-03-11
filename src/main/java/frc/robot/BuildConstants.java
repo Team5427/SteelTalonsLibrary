9e7fdd61424ff86f0d15800dfcf52b2db4ec1ab6
AaronZhenger <alock8mini@gmail.com>
2026-03-10 23:26:33 -0500

    Update Vendor Deps

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,14 +3,14 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "SteelTalonsLibrary-2026";
+  public static final String MAVEN_NAME = "SteelTalonsLibrary";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 64;
-  public static final String GIT_SHA = "e390426b5429a1ab8f5612f941333dff7d6a61fc";
-  public static final String GIT_DATE = "2026-01-22 00:46:03 CST";
-  public static final String GIT_BRANCH = "2026-V2";
-  public static final String BUILD_DATE = "2026-01-22 00:47:53 CST";
-  public static final long BUILD_UNIX_TIME = 1769064473414L;
+  public static final int GIT_REVISION = 66;
+  public static final String GIT_SHA = "6b42d20be559e49a0871b1b59a0adc00875bd2be";
+  public static final String GIT_DATE = "2026-01-22 00:49:03 CST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-03-10 23:25:13 CDT";
+  public static final long BUILD_UNIX_TIME = 1773203113885L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
